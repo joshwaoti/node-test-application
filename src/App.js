fcacd3b734eb6ccb6ba3ffafb956b968c4c677aa
joshwaoti <73524531+joshwaoti@users.noreply.github.com>
2023-07-18 21:42:51 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="flex flex-1">
+      <div className="flex flex-1 flex-col justify-center items-center">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
